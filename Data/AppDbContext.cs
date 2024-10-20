607aa8264f5daae621a34be4dd0a36bb67ec75e4
JonF12 <78042059+JonF12@users.noreply.github.com>
2024-10-20 16:44:21 +0100

    Uppercase path in AppDbContext for unix based systems

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -9,7 +9,7 @@ namespace DotNetHtmxTypescriptTemplate.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlite("Data Source=./data/App.db");
+            optionsBuilder.UseSqlite("Data Source=./Data/App.db");
         }
     }
 }
